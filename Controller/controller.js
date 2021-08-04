008a8cb717d6308a36f6c8ca8dbd57d73c552395
fadlyhdiwjya <wijayafadlyhadi28@gmail.com>
2021-08-04 19:28:05 +0700

    create controller readProducts

--- Controller/controller.js
+++ Controller/controller.js
@@ -13,11 +13,23 @@ exports.createProducts = ( req, res ) => {
         })
 
         product.save().then( data => {
-            res.send(data)
+            res.send({ message: "Success, data was add!"})
         }).catch(err => {
             res.status(500).send({
                 message: "Sorry, data failed add"
             })
+        })
+    }
+}
+
+exports.readProducts = (req , res ) => {
+    if(!req.body){
+        res.send({ message: "error!"})
+    } else {
+        const product = productsDB.find().then(data => {
+            res.send(data)
+        }).catch(err => {
+            res.status(500).send({message: "Sorry, data error"});
         })
     }
 }
